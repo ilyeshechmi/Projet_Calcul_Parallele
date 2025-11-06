a3122b68d00ef2319f011526f76dcc9eb26c5460
Ynssa <younoussa.idjabou@bordeaux-inp.fr>
2025-11-06 21:02:03 +0100

    ajout du calcul de l'erreur

--- fonctions.f90
+++ fonctions.f90
@@ -9,5 +9,38 @@ contains
 
         res = exp(-(x-5._pr)**2)
     end function gaussienne
+    
+ function erreur_L2(u_exact, u_num, dx) result(errL2)
+    implicit none
+    real(pr), intent(in) :: u_exact(:), u_num(:)
+    real(pr), intent(in) :: dx
+    real(pr) :: errL2
+    real(pr) :: sum,exact
+    integer  :: i 
+
+    sum=0._pr
+    exact=0._pr
+    do i = 1,size(u_exact)
+        sum = sum+ dx*(u_exact(i)-u_num(i))**2
+        exact = exact+ dx*(u_exact(i))**2
+    end do
+    
+    errL2=sqrt(sum/exact)
+
+  end function erreur_L2
+
+
+  function erreur_Linf(u_exact, u_num) result(errLinf)
+    implicit none
+    real(8), intent(in) :: u_exact(:), u_num(:)
+    real(8) :: errLinf
+    real(8) :: max_err, max_u
+
+    max_err = maxval( abs(u_exact - u_num) )
+    max_u   = maxval( abs(u_exact) )
+
+    errLinf = max_err / max_u
+
+  end function erreur_Linf
 
 end module 
--- main.f90
+++ main.f90
@@ -9,8 +9,10 @@ program advection_rusanov
   type(Parametres) :: params
   integer  :: nx, nsteps, n, save_every, i
   real(pr) :: L, a, dt, Tfinal, dx, t, cfl, uG
-  real(pr), allocatable :: x(:), u(:)
-
+  real(pr), allocatable :: x(:), u(:),u_ex(:)
+  real(pr) ::  errL2, errLinf
+  
+  
   ! --- Lecture paramètres ---
   call lire_parametres("parametres.txt", params)
   nx         = params%nx
@@ -52,5 +54,27 @@ program advection_rusanov
   write(*,*) "Terminé. Fichiers générés: "//trim(params%outfile)//"_t=<t>.dat"
   write(*,*) "============================================================"
 
-  deallocate(x, u)
+  ! Calcul de l'erreur
+  allocate(u_ex(nx))
+  do i = 1, nx
+    if (x(i) - a*t > 0._pr) then
+      u_ex(i) = exp(-(x(i) - a*t-5._pr)**2)
+    else
+      u_ex(i) = uG
+    end if
+  end do
+
+  errL2   = erreur_L2(u_ex, u, dx)
+  errLinf = erreur_Linf(u_ex, u)
+
+  open(unit=20, file=trim(params%outfile)//'_erreurs.dat', status='replace', action='write')
+  write(20, '(A, F12.6)') 'Erreur L2   = ', errL2
+  write(20, '(A, F12.6)') 'Erreur Linf = ', errLinf
+  close(20)
+  write(*,*) 'Erreurs calculées et sauvegardées dans ', trim(params%outfile)//'_erreurs.dat'
+
+
+
+  deallocate(x, u,u_ex)
+
 end program advection_rusanov
